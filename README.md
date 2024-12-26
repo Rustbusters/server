8f32497249eb6a3dd7394382fe7829e7478fc5a0
Ilya Emeliyanov <ilya@MacBookPro.fritz.box>
2024-12-26 16:54:21 +0100

    Update README.md: add UI explanation

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# Server
+# Introduction
 This is the server implementation for the Advanced Programming Course 2024-2025 at the University of Trento.\
-The repository provides the implementation both for the Communication and Text/Media servers.
+This repository provides the implementation for the Communication server.
+
+# Overview
+The server implementation is organized in 2 main feature blocks:
+
+- Standard: basic features of Packet Handling and Network Discovery
+- Custom: more advanced custom features that include:
+    - Load balancer: server requests are split among multiple worker threads
+    - Persistency: client messages are saved in a DB
+    - UI: user interface for interacting with the server and seeing statistics
+
+## Load balancer
+
+## Persistency
+
+## UI
+The UI is implemented through the combination of:
+
+- **WebSocket Server**: used for full-duplex communication between the servers + http client and the WebSocket Server.
+- **HTTP Server**: allows the hosting of a landing page with the UI.
+
+![[./assets/diagram.png]]
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -8,23 +8,23 @@ mod ad;
 use crate::server::stats::Stats;
 use commands::HostCommand;
 use crossbeam_channel::{Receiver, Sender};
+use petgraph::prelude::GraphMap;
+use petgraph::Undirected;
 use log::{error, info};
-use rand::{rng, Rng};
 use std::collections::{HashMap};
 use wg_2024::network::NodeId;
-use wg_2024::packet::NodeType::{Drone};
 use wg_2024::packet::{Fragment, NodeType, Packet};
 use crate::commands::HostEvent;
+use rand::*;
 
 pub struct RustBustersServer {
     id: NodeId,
-    node_type: NodeType,
     controller_send: Sender<HostEvent>,
     controller_recv: Receiver<HostCommand>,
     packet_recv: Receiver<Packet>,
     packet_send: HashMap<NodeId, Sender<Packet>>,
     known_nodes: HashMap<NodeId, NodeType>,
-    topology: HashMap<NodeId, Vec<NodeId>>,
+    topology: GraphMap<NodeId, f32, Undirected>,
     flood_id_counter: u64,
     session_id_counter: u64,
     // (session_id, fragment_index) -> packet
@@ -32,52 +32,31 @@ pub struct RustBustersServer {
     // session_id -> (fragments, num_fragments) (u8 is the number of fragments received) (for reassembly)
     pending_received: HashMap<u64, (Vec<Option<Fragment>>, u64)>,
     stats: Stats,
-    echo_mode: bool,
-    auto_send: bool,
-    auto_send_interval: u64, // in ms
 }
 
 impl RustBustersServer {
     pub fn new(
         id: NodeId,
-        node_type: NodeType,
         controller_send: Sender<HostEvent>,
         controller_recv: Receiver<HostCommand>,
         packet_recv: Receiver<Packet>,
         packet_send: HashMap<NodeId, Sender<Packet>>,
     ) -> Self {
-        if let Drone = node_type {
-            error!("Drone nodes are not supported by RustBustersServer");
-            panic!("Drone nodes are not supported by RustBustersServer");
-        }
-        info!("Host {} spawned succesfully", id);
+        info!("Server {} spawned succesfully", id);
         Self {
             id,
-            node_type,
             controller_send,
             controller_recv,
             packet_recv,
             packet_send,
             known_nodes: HashMap::new(),
-            topology: HashMap::new(),
+            topology: GraphMap::new(),
             flood_id_counter: rng().random_range(1000..=2000),
             session_id_counter: rng().random_range(100..=200),
             pending_sent: HashMap::new(),
             pending_received: HashMap::new(),
             stats: Stats::default(),
-            echo_mode: false,
-            auto_send: false,
-            auto_send_interval: 0,
         }
-    }
-
-    pub fn auto_send_on(&mut self, interval: u64) {
-        self.auto_send = true;
-        self.auto_send_interval = interval;
-    }
-
-    pub fn auto_send_off(&mut self) {
-        self.auto_send = false;
     }
 
     pub fn run(&self) {
@@ -86,17 +65,21 @@ impl RustBustersServer {
         self.discover_network();
 
         // Loop over and listen for incoming messages
+        todo!();
+        loop {
+
+        }
     }
     
     pub(crate) fn send_to_sc(&mut self, event: HostEvent) {
         if self.controller_send.send(event).is_ok() {
             info!(
-                "Node {} - Sent NodeEvent to SC",
+                "Server {} - Sent NodeEvent to SC",
                 self.id
             );
         } else {
             error!(
-                "Node {} - Error in sending event to SC",
+                "Server {} - Error in sending event to SC",
                 self.id
             );
         }
