48f0801b5d68741efb286e881e5c97866fcc391a
Ilya Emeliyanov <ilya@MacBookPro.fritz.box>
2025-02-08 14:26:02 +0100

    on ErrorInRouting: update network topology + resend fragment

--- src/server/packet/nack_handler.rs
+++ src/server/packet/nack_handler.rs
@@ -2,8 +2,10 @@ use crate::{RustBustersServer, StatsManager};
 use common_utils::HostEvent;
 use common_utils::{PacketHeader, PacketTypeHeader};
 use log::{info, warn};
+use wg_2024::network::SourceRoutingHeader;
 use wg_2024::packet::NackType;
 use wg_2024::packet::NackType::Dropped;
+use wg_2024::packet::{Fragment, NodeType, Packet, PacketType};
 
 impl RustBustersServer {
     pub(crate) fn handle_nack(
@@ -41,7 +43,42 @@ impl RustBustersServer {
                             "Server {}: Nack for fragment {} with type {:?}",
                             self.id, fragment_index, nack_type
                         );
+
+                        // Updating topology
                         self.topology.remove(&drone_id);
+                        // Removing from known nodes
+                        self.known_nodes.remove(&drone_id);
+
+                        // Calculating new route and resending fragment
+                        let dest_id = *packet.routing_header.hops.last().expect("No destination");
+                        if let Some(route) = self.find_route(dest_id) {
+                            let mut new_packet = packet.clone();
+                            new_packet.routing_header.hops = route.clone();
+                            new_packet.routing_header.hop_index = 1;
+
+                            // Send the packet
+                            if let Some(sender) =
+                                self.packet_send.get(&new_packet.routing_header.hops[1])
+                            {
+                                if let Err(err) = sender.send(new_packet.clone()) {
+                                    warn!(
+                                        "Server {}: Unable to resend fragment {}: {}",
+                                        self.id, fragment_index, err
+                                    );
+                                } else {
+                                    StatsManager::inc_message_fragments_sent(self.id);
+
+                                    // Send MsgFragment to Simulation Controller
+                                    self.send_to_sc(HostEvent::PacketSent(PacketHeader {
+                                        session_id,
+                                        pack_type: PacketTypeHeader::MsgFragment,
+                                        routing_header: new_packet.routing_header.clone(),
+                                    }));
+                                }
+                            }
+                        } else {
+                            warn!("Server {}: Error in finding route", self.id);
+                        }
                     }
                     NackType::DestinationIsDrone | NackType::UnexpectedRecipient(_) => {
                         warn!(
